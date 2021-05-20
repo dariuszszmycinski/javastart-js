09f5bf10dda419b979bb69d1dc8f57e274c8cf43
dariuszszmycinski <68079317+dariuszszmycinski@users.noreply.github.com>
2021-05-20 13:12:00 +0200

    Add files via upload

---
+++ spring-angular-http/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO `test`.`product` (`kcal`, `name`, `producer`) VALUES ('222', 'Pepsi', 'Pepsi Co');
+INSERT INTO `test`.`product` (`kcal`, `name`, `producer`) VALUES ('123', 'Czekolada mleczna', 'Milka');
