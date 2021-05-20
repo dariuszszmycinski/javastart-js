3e7ad21ff68c10790f585657f89fc70c46823581
dariuszszmycinski <68079317+dariuszszmycinski@users.noreply.github.com>
2021-05-20 13:14:05 +0200

    Add files via upload

---
+++ spring-angular-routing/src/main/java/dasz/repository/BookRepository.java
@@ -0,0 +1,8 @@
+package dasz.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import dasz.model.Book;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+}
