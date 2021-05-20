896643665425d9a259b02398dc2eea473d813fe0
dariuszszmycinski <68079317+dariuszszmycinski@users.noreply.github.com>
2021-05-20 13:13:00 +0200

    Add files via upload

---
+++ spring-angular-resource/src/main/java/dasz/controller/ProductController.java
@@ -0,0 +1,49 @@
+package dasz.controller;
+
+import dasz.model.Product;
+import dasz.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public ProductController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Product>> allProducts() {
+        List<Product> allProducts = productRepository.findAll();
+        return ResponseEntity.ok(allProducts);
+    }
+
+    @GetMapping(path = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> saveProduct(@RequestBody Product product){
+        Product save = productRepository.save(product);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(save.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(save);
+    }
+
+}
